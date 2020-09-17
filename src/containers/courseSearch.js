14ffcfe39f8eb243d20c800af4eec695a2937fc6
Javkhaa00 <liplicker0423@gmail.com>
2020-09-17 11:57:00 +0800

    feat: git push

--- src/containers/courseSearch.js
+++ src/containers/courseSearch.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Select from 'react-select'
 
 const CourseSearch = () => {
+
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
+
+    const BrowseSelect = () => (
+        <Select options={options} />
+    )
+    const CourseSelect = () => (
+        <Select options={options} />
+    )
+
     return (
         <div className="course-search">
-            <h1>DSADAS</h1>
+            <div className="course-search--wrapper">
+                <div className="course-search--wrapper--header">
+                    <div className="course-search--wrapper--header--box">
+                        <span className="course-search--wrapper--header--box--title">
+                            Browse course by us State:
+                        </span>
+                        <div className="course-search--wrapper--header--box--select" >
+                            <BrowseSelect />
+                        </div>
+                    </div>
+                    <div className="course-search--wrapper--header--box">
+                        <span className="course-search--wrapper--header--box--title">
+                            Browse course by us State:
+                        </span>
+                        <div className="course-search--wrapper--header--box--select" >
+                            <CourseSelect />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
