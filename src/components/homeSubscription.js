796a35d08db38a945ca5b62dfbcdb7a2368bc910
Javkhaa00 <liplicker0423@gmail.com>
2020-09-18 16:07:46 +0800

    feat: changed html elements div to template

--- src/components/homeSubscription.js
+++ src/components/homeSubscription.js
@@ -8,29 +8,29 @@ const HomeSubscription = () => {
             <span className="subs--desc">
                 Monotonectally iterate best-of-breed customer service whereas low-risk high-yield imperatives. Holisticly leverage existing.
             </span>
-            <template className="subs--wrapper">
-                <div className="subs--wrapper--side">
+            <div className="subs--wrapper">
+                <template className="subs--wrapper--side">
                     <span className="subs--wrapper--side--title">Free</span>
                     <span className="subs--wrapper--side--desc">(free 3 courses)</span>
                     <span className="subs--wrapper--side--amount">$0.00</span>
                     <div className="subs--wrapper--side--subscribe">Subscribe</div>
-                </div>
-                <div className="subs--wrapper--main">
+                </template>
+                <template className="subs--wrapper--main">
                     <span className="subs--wrapper--main--popular">Most popular</span>
                     <span className="subs--wrapper--side--title">One Time</span>
                     <span className="subs--wrapper--side--desc">(life of phone)</span>
                     <span className="subs--wrapper--side--amount">$29.00</span>
                     <div className="subs--wrapper--main--subscribe">Subscribe</div>
-                </div>
-                <div className="subs--wrapper--side">
+                </template>
+                <template className="subs--wrapper--side">
                     <span className="subs--wrapper--side--title">Life Time</span>
                     <span className="subs--wrapper--side--desc">
                         (transfers to any supported phone)
                     </span>
                     <span className="subs--wrapper--side--amount">$85.00</span>
                     <div className="subs--wrapper--side--subscribe">Subscribe</div>
-                </div>
-            </template>
+                </template>
+            </div>
         </div>
     );
 };
