cfa9fe04f6ede13868247c7a70f3e1c8ca73dc29
Javkhaa00 <liplicker0423@gmail.com>
2020-10-02 09:54:51 +0800

    feat: final xD

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
